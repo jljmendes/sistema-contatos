c10af9d46b3c12929a3629529f341d78edf7ee32
jocabispo <jljmendesmanutencao@gmail.com>
2022-03-20 22:39:49 -0300

    Colocando imagem de fundo

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ if($sql->rowCount() > 0){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <div class="container">
